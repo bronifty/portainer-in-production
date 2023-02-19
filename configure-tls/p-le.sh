40c314707008dd75bc6e0f739e99849189494705
bronifty <bronifty@gmail.com>
2023-02-19 05:40:43 -0500

    update configs

--- configure-tls/p-le.sh
+++ configure-tls/p-le.sh
@@ -5,8 +5,8 @@ docker run -d -p 443:9443 -p 80:8000 \
     --name portainer --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    -v /etc/letsencrypt/live/portainer-gcp.bronifty.xyz:/certs/live/portainer-gcp.bronifty.xyz:ro \
-    -v /etc/letsencrypt/archive/portainer-gcp.bronifty.xyz:/certs/archive/portainer-gcp.bronifty.xyz:ro \
+    -v /etc/letsencrypt/live/portainer.bronifty.xyz:/certs/live/portainer.bronifty.xyz:ro \
+    -v /etc/letsencrypt/archive/portainer.bronifty.xyz:/certs/archive/portainer.bronifty.xyz:ro \
     portainer/portainer-ce:latest \
-    --sslcert /certs/live/portainer-gcp.bronifty.xyz/fullchain.pem \
-    --sslkey /certs/live/portainer-gcp.bronifty.xyz/privkey.pem
+    --sslcert /certs/live/portainer.bronifty.xyz/fullchain.pem \
+    --sslkey /certs/live/portainer.bronifty.xyz/privkey.pem
