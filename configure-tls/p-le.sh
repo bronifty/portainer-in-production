0de10ea31b245ed10c63a35f56697bd67112d2bd
bronifty <bronifty@gmail.com>
2023-02-19 04:55:06 -0500

    updating the mapped volume portainer docker ports to 80 and 443

--- configure-tls/p-le.sh
+++ configure-tls/p-le.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 set -euxo pipefail
 
-docker run -d -p 9443:9443 -p 8000:8000 \
+docker run -d -p 443:9443 -p 80:8000 \
     --name portainer --restart always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    -v /etc/letsencrypt/live/p.rawkode.academy:/certs/live/p.rawkode.academy:ro \
-    -v /etc/letsencrypt/archive/p.rawkode.academy:/certs/archive/p.rawkode.academy:ro \
+    -v /etc/letsencrypt/live/portainer-gcp.bronifty.xyz:/certs/live/portainer-gcp.bronifty.xyz:ro \
+    -v /etc/letsencrypt/archive/portainer-gcp.bronifty.xyz:/certs/archive/portainer-gcp.bronifty.xyz:ro \
     portainer/portainer-ce:latest \
-    --sslcert /certs/live/p.rawkode.academy/fullchain.pem \
-    --sslkey /certs/live/p.rawkode.academy/privkey.pem
+    --sslcert /certs/live/portainer-gcp.bronifty.xyz/fullchain.pem \
+    --sslkey /certs/live/portainer-gcp.bronifty.xyz/privkey.pem
